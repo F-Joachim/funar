3fce509282e5ac4bab11133a8229a5f4879c6355
Mike Sperber <sperber@deinprogramm.de>
2026-05-05 11:26:22 +0200

    Update.

--- vorkurs/data.rkt
+++ vorkurs/data.rkt
@@ -55,11 +55,32 @@
 (define-record time ; Signatur
   make-time ; Konstruktor
   (time-hour   natural) ; "natürliche Zahlen" 0,1,2,3,4,5...
-  (time-minute natural))
+  (time-minute natural)) ; Selektor / "Getter-Funktion"
 
 (: make-time (natural natural -> time))
+(: time-hour (time -> natural))
+(: time-minute (time -> natural))
 
 ; 11 Uhr 15 Minuten
 (define time1 (make-time 11 15))
 ; 14:23
 (define time2 (make-time 14 23))
+
+; Minuten seit Mitternacht
+(: msm (time -> natural))
+
+(check-expect (msm time1)
+              675)
+(check-expect (msm time2)
+              863)
+
+; Schablone
+#;(define msm
+  (lambda (time)
+    ... (time-hour time) ...
+    ... (time-minute time) ...))
+
+(define msm
+  (lambda (time)
+    (+ (* (time-hour time) 60)
+       (time-minute time))))
