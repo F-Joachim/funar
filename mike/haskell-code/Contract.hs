4d60f9b21e188737cff9df250f10f164fb7781bf
Mike Sperber <sperber@deinprogramm.de>
2026-05-06 14:21:59 +0200

    Update.

--- mike/haskell-code/Contract.hs
+++ mike/haskell-code/Contract.hs
@@ -1,1 +1,20 @@
 module Contract where
+
+-- Auftrag: komplexe Finanzverträge als Daten repräsentieren
+-- Vertrag zwischen 2 Parteien, eine davon "wir"
+
+{-
+Vorgehensweise:
+
+- einfaches Beispiel
+  "Ich bekomme Weihnachten 100€."
+
+-}
+
+data Date = MkDate String -- ISO-Format
+  deriving (Show, Eq, Ord)
+
+xmas = MkDate "2026-12-24"
+
+data Contract =
+    ZeroCouponBond Date Amount Currency
