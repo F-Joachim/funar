30e1f47d7c7266b2aed1afaeff96efb2337baa8e
Mike Sperber <sperber@deinprogramm.de>
2026-05-06 10:02:20 +0200

    Update.

--- mike/haskell-code/Intro.hs
+++ mike/haskell-code/Intro.hs
@@ -1,3 +1,24 @@
 {-# LANGUAGE InstanceSigs #-}
 module Intro where
 
+x :: Integer
+x = 23 + 2 * 21
+
+y :: Integer
+y = x + 5
+
+-- Kommentar
+
+-- >>> x 
+-- 65
+
+-- Zahl verdoppeln
+double :: Integer -> Integer
+
+-- >>> double 23
+-- 46
+
+-- double = \ x -> x * 2
+double x = x * 2 -- syntaktischer Zucker
+
+--
