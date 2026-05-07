14491630d261b9c44b27db0cd571a1819c6f450a
Mike Sperber <sperber@deinprogramm.de>
2026-05-07 12:08:15 +0200

    Update.

--- mike/haskell-code/Validation.hs
+++ mike/haskell-code/Validation.hs
@@ -1,1 +1,27 @@
 module Validation where
+
+data Car = MkCar { licensePlate :: String,
+                   seatCount :: Integer }
+    deriving Show
+
+-- blöd:
+car1 = MkCar "" (-1)
+
+data Result a =
+    Success a
+  | Failure [String] -- Fehlermeldung
+  deriving Show
+
+instance Functor Result where
+    fmap :: (a -> b) -> Result a -> Result b
+    fmap f (Succes)
+
+mkCar :: String -> Integer -> Result Car
+mkCar s n =
+    if length s >= 2 && length s <= 14
+    then if n >= 2
+         then Success (MkCar s n)
+         else Failure ["invalid seat count"]
+    else if n >= 2
+         then Failure ["invalid license-plate length"]
+         else Failure ["invalid seat count", "invalid license-plate length"]
